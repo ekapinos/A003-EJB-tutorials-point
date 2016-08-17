a40c3f340a8c8c23cb6de6e7cc6a02d34966ce00
Evgeniy Kapinos <evgeniy.kapinos@gmail.com>
2016-08-17 09:57:46 +0300

    Fixed Maven ejb module

---
+++ EJB-tutorials-point-ejb/src/main/java/local/kapinos/Temp.java
@@ -0,0 +1,18 @@
+package local.kapinos;
+
+import javax.ejb.Singleton;
+
+/**
+ * Session Bean implementation class Temp
+ */
+@Singleton
+public class Temp {
+
+    /**
+     * Default constructor. 
+     */
+    public Temp() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
