d9341984a7fd35b412d2100a9f5fc86462d286cb
Evgeniy Kapinos <evgeniy.kapinos@gmail.com>
2016-08-27 23:18:38 +0300

    Added Persistance listeners

--- install-a003-ejb-tutorials-point.cmd
+++ install-a003-ejb-tutorials-point.cmd
@@ -1,3 +1,4 @@
+call asadmin start-database
 call asadmin create-jms-resource --restype javax.jms.Queue --property Name=PhysicalQueue jms/BookQueue
 call asadmin create-jms-resource --restype javax.jms.ConnectionFactory jms/BookQueueFactory
 pause
--- uninstall-a003-ejb-tutorials-point.cmd
+++ uninstall-a003-ejb-tutorials-point.cmd
@@ -1,3 +1,4 @@
-call asadmin delete-jms-resource jms/BookQueue
 call asadmin delete-jms-resource jms/BookQueueFactory
+call asadmin delete-jms-resource jms/BookQueue
+call asadmin stop-database
 pause
